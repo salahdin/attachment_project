91a1084b5c67ffc51a76962a0621398c75f46a7b
salahdin zeberga <salahdinzeberga@admins-MacBook-Air-7.local>
2019-07-09 16:02:41 +0200

    added new templates

--- protocol_registration_system/protocol/urls.py
+++ protocol_registration_system/protocol/urls.py
@@ -3,6 +3,7 @@ from .import views
 
 app_name="protocol"
 urlpatterns = [
-    path('apply', views.apply, name='apply'),
-    path('list', views.ProtocolRequestListView.as_view(), name='protocol-request-list',)
-    ]
+    path('apply/', views.apply, name='apply'),
+    path('list/', views.ProtocolRequestListView.as_view(), name='protocol-request-list',),
+    path('detail/<int:id>', views.ProtocolRequestDetailView.as_view(), name='protocol-request-detail',)
+]
--- protocol_registration_system/protocol/views.py
+++ protocol_registration_system/protocol/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 import datetime 
 from .form import ProtocolRequestForm
 from django.core.mail import send_mail
 from django.contrib import messages
 from .import email
-from .models import ProtocolRequest
+from .models import ProtocolRequest,ProtocolResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 
 # email api key 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
@@ -13,21 +15,34 @@ from django.views.generic.list import ListView
 
 class ProtocolRequestListView(ListView):
     model = ProtocolRequest
-    paginate_by = 20
+    paginate_by = 5
     context_object_name = 'ProtocolRequest'
     template_name = 'protocol/list.html'
 
+
     def get_queryset(self):
         return ProtocolRequest.objects.filter(request_date__lte=datetime.date.today()).order_by('-request_date')[:5]
+
+
+class ProtocolRequestDetailView(DetailView):
+
+    template_name = 'protocol/detail.html'
+    queryset = ProtocolRequest.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(ProtocolRequest,id=id_)
 
 
 
 def apply(request):
     if request.method == "POST":
         form = ProtocolRequestForm(request.POST)
+
         if form.is_valid():
             post=form.save(commit=False)
             post.request_date=datetime.date.today()#request_date inserted before saving
+
             """
             send_mail(
                 email.req_subject,
@@ -39,7 +54,7 @@ def apply(request):
             """
 
             post.save()
-            #create a response for the new
+            # create a response for the new
             ProtocolResponse.objects.create(response=post,number=00)
             messages.success(request, 'Form submission successful!')
 
